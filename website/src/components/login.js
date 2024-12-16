30601a3afcc1de96c313d696bb64bccb5c399269
ashleygp2002 <106784163+ashleygp2002@users.noreply.github.com>
2024-12-15 18:07:10 -0800

    Update in Register & Login page

--- website/src/components/login.js
+++ website/src/components/login.js
@@ -1,22 +1,49 @@
-import React from 'react';
+// import React from 'react';
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import './login.css';
-
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch('http://127.0.0.1:5000/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password }),
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert(data.message);
+            navigate('/catalog');
+           
+        } else {
+            alert(data.error);
+        }
+    };
+
     return (
         <div className="login-container">
+             <div className="login-box">
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleLogin}>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" />
+                <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
                 
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" />
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 
                 <button type="submit">Login</button>
                 <button type="button" onClick={() => window.location.href = '/register'}>
                     Register
                 </button>
             </form>
+            </div>
         </div>
     );
 };
--- website/src/components/register.js
+++ website/src/components/register.js
@@ -1,21 +1,46 @@
-import React from 'react';
+// import React from 'react';
+import React, {useState} from 'react';
 import './register.css';
 
 const Register = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch('http://127.0.0.1:5000/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password }),
+        });
+        
+        const data = await response.json();
+        if (response.ok) {
+            alert(data.message);
+            window.location.href = '/';
+        }
+        else {
+            alert(data.error);
+        }
+    };
+
     return (
         <div className="register-container">
+            <div className='register-box'>
             <h1>Register</h1>
-            <form>
+            <form onSubmit={handleRegister}>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" />
+                <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
                 
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" />
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 
-                <button type="button" onClick={() => window.location.href = '/login'}>
+                <button type="submit" onClick={() => window.location.href = '/'}>
                     Register
                 </button>
             </form>
+            </div>
         </div>
     );
 };
